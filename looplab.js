de7bfe2f7adb68eec3aa14bd1ac73991c176a480
SuzanHiary <>
2021-05-30 23:13:23 +0000

    adde kids number

--- looplab.js
+++ looplab.js
@@ -1,10 +1,11 @@
+
 var userName = prompt ("Whats your name ?");
 alert("welcome to our websit " + userName );
 
 
 
  //img = '';
- var flowertype = prompt ("What yours Fav flowers type ?");
+ var flowertype = prompt ("What yours Fav flowers type(tulip) ?");
  if (flowertype == "tulip"){
   document.write('<img src="https://www.ikea.com/jo/en/images/products/smycka-artificial-bouquet-in-outdoor-tulip__0797543_pe767312_s5.jpg?f=s"/>')
 } ; 
@@ -32,14 +33,16 @@ while (starRating==0){
 
 //images loop
 // var c;
-c = prompt ("How many kids you have?");
-for(var c = 0; c <= 5; c) {
-  if (c==1 ){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />');} ;  if(c==2){for(c==1;c<=2;c++){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />'); } } ;
-
-   if(c==3){for(c==1;c<=3;c++){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />'); };} 
-
-  if(c==4){for(c==1;c<=4;c++){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />'); };} 
-} ;
+c = prompt ("How many kids do you have?");
+ 
+  if (c==1 ){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />');} ;  
+  //break;
+  
+  if(c==2){for(c==0;c<=2;c--){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />'); } } ;
+//break;
+   if(c==3){for(c==0;c<=3;c--){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />'); };} 
+//break;
+  if(c==4){for(c==0;c<=4;c--){document.write('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />'); };}  ;
 
 var userAge = prompt('please enter your Age ?')
 //if(userName == 'Suzan' && userAge == 25) { 
