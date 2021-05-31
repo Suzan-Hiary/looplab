7bb0edbb12f64813005c2e8b47858022d39f48dc
SuzanHiary <>
2021-05-31 12:50:57 +0000

    added functions

---
+++ functions.js
@@ -0,0 +1,58 @@
+
+
+  
+  
+function userinformayion () { let userName = prompt('Please enter your name');
+ let userAge = prompt('Please enter your Age');
+ 
+ //img = '';
+ let flowertype = prompt ("What yours Fav flowers type(tulip) ?");
+ if (flowertype == "tulip"){
+  document.write('<img src="https://www.ikea.com/jo/en/images/products/smycka-artificial-bouquet-in-outdoor-tulip__0797543_pe767312_s5.jpg?f=s"/>');
+ }
+
+ }
+
+userinformayion (); 
+
+//var starRating = prompt ("What star rating would you give, 1-5?");
+//if the starRating equal 1 loop again
+//while (starRating==1){
+  //consol.log('inside the while loop');
+  
+ // starRating=prompt("please Give us star rating !");}
+  //if the starRating equal 2 loop again
+//while (starRating==2){
+  //consol.log('inside the while loop');
+  
+  //starRating=prompt("please Give us star rating !");
+//}
+//if the starRating equal 0 loop again
+//while (starRating==0){
+  //consol.log('inside the while loop');
+  
+  //starRating=prompt("please Give us star rating !"); }
+ 
+
+//images loop;
+// var c;
+//var k ;
+c = prompt ("How many kids do you have?");
+for(k=1 ; k <= c ; k++){ document.write ('<img src="https://img.freepik.com/free-photo/newborn-toddler-boy-laughing-bed_115594-1502.jpg?size=626&ext=jpg&ga=GA1.2.1983713001.1615680000" />');}
+//brek ; 
+ 
+  
+
+
+//if(userName == 'Suzan' && userAge == 25) { 
+   //console.log(true); 
+// }
+
+
+
+
+
+//var input = prompt ("did the page was useful? ");
+  // if(input=="yes"){ input=prompt ("Do You Want To Sign In Our Club?");
+  // }
+ //else if (input=="no") { input=prompt ("Thank You For visiting^__^ ") } ;
